b9bedb157173e3a8d6ac69e293f7bfc76cd054ef
mefforbn <bryce.mefford@outlook.com>
2025-11-19 16:39:09 -0600

    created count_words function

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("greetings boots")
+def get_book_text(file_path):
+
+	with open(file_path) as f:
+		file_innards = f.read()
+
+	return file_innards
+
+def count_words(content):
+
+	book_list = content.split()
+	count = 0
+
+	for i in range(0, len(book_list)):
+		count += 1
+
+	return count
+
+
+def main():
+	count = count_words(get_book_text("books/frankenstein.txt"))
+	print("Found " + str(count) + " total words")
+
+
+main()
