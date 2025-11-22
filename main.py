6f30a7d8bedc158e6fdc2a53808378f22dc3b5ed
mefforbn <bryce.mefford@outlook.com>
2025-11-22 12:45:30 -0600

    Update to file path

--- main.py
+++ main.py
@@ -5,14 +5,21 @@ def get_book_text(file_path):
 
 	return file_innards
 
+import sys
+
 from stats import count_words, count_characters, sorted_characters
 
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
+
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    text = get_book_text(sys.argv[1])
 
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {sys.argv[1]}...")
 
     count = count_words(text)
     print("----------- Word Count ----------")
@@ -25,5 +32,8 @@ def main():
     for item in sorted_list:
         print(f"{item['char']}: {item['num']}")
     print("============= END ===============")
+
+    
+
 
 main()
